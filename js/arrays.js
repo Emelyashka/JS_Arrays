adadc5763a5c9c264b2d636f505ca93ed4734d5e
Emelyashka <volk.ea@bk.ru>
2023-10-31 14:15:53 +0300

    Задача 7 решена

--- js/arrays.js
+++ js/arrays.js
@@ -40,3 +40,12 @@ function task6()
     arr.push(elem);
     console.log(arr.toString());
 }
+
+function task7()
+{
+    let arr=Array.from(Array(10),()=>{return Math.round(Math.random()*20)});
+    console.log(arr.toString());
+    let summaChetn=arr.reduce((sum,elem,index)=>{if (index%2==0)sum=sum+elem; return sum;},0);
+    let summaNechetn=arr.reduce((sum,elem,index)=>{if (index%2!=0) sum=sum+elem; return sum;},0);
+    console.log(summaChetn/summaNechetn);
+}
